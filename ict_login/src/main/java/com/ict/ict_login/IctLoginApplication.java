82f907d1eb4ca495c694213b3c46a9f58f1be0ec
wangsr <12345678>
2021-02-08 14:22:04 +0800

    login提交

---
+++ ict_login/src/main/java/com/ict/ict_login/IctLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ict.ict_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IctLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IctLoginApplication.class, args);
+    }
+
+}
---
+++ ict_login/src/test/java/com/ict/ict_login/IctLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.ict_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IctLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
