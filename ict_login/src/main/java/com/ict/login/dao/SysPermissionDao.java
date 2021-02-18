d6002958fe1952e2f18f1475cd194a598aa55fff
wangsr <12345678>
2021-02-18 16:34:37 +0800

    错误sql修改

--- ict_login/src/main/java/com/ict/login/dao/SysPermissionDao.java
+++ ict_login/src/main/java/com/ict/login/dao/SysPermissionDao.java
@@ -2,6 +2,7 @@ package com.ict.login.dao;
 
 import com.ict.login.entity.Permission;
 import com.ict.login.entity.SysPermission;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
  * @Date 2021/2/18
  * @Version 1.0
  */
+@Mapper
 public interface SysPermissionDao {
     List<SysPermission> selectListByPath(@Param("url")String url);
 }
