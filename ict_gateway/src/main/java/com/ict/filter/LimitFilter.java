c8d1c2a0cf07f53f18512903004234c7793dc41a
wangsr <12345678>
2021-03-02 17:43:14 +0800

    gateway 路由配置以及注册到nacos

--- ict_gateway/src/main/java/com/ict/filter/LimitFilter.java
+++ ict_gateway/src/main/java/com/ict/filter/LimitFilter.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
--- ict_modeltrain/src/main/java/com/ict/IctModeltrainApplication.java
+++ ict_modeltrain/src/main/java/com/ict/IctModeltrainApplication.java
@@ -2,8 +2,10 @@ package com.ict;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IctModeltrainApplication {
 
     public static void main(String[] args) {
