f3165232863222802817840c3b739e7dfd04007f
wangsr <12345678>
2021-03-15 18:05:47 +0800

    ict_samplemanage  nacos配置中心

--- ict_samplemanage/src/main/java/com/ict/ictsamplemanage/controller/UserController.java
+++ ict_samplemanage/src/main/java/com/ict/ictsamplemanage/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ict.ictsamplemanage.controller;
 
 import com.ict.ictsamplemanage.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,17 @@ public class UserController {
     @Autowired
     private UserServiceImpl userService;
 
+    @Value("${test.username}")
+    private String username;
+
     @GetMapping("/userInfo")
     public Map<String, Object> userVa() throws Exception {
+        System.out.println(username);
         return userService.getUserInfo(1);
+    }
+
+    @GetMapping("/userInfo2")
+    public String userVa2() throws Exception {
+        return "success";
     }
 }
