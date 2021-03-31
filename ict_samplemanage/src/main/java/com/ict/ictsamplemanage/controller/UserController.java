f665d433de0e7dc89f14cafaf74bd060c4875549
wangsr <12345678>
2021-03-31 17:49:30 +0800

    nacos配置中心

--- ict_samplemanage/src/main/java/com/ict/ictsamplemanage/controller/UserController.java
+++ ict_samplemanage/src/main/java/com/ict/ictsamplemanage/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ict.ictsamplemanage.controller;
 import com.ict.ictsamplemanage.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController {
     @Autowired
     private UserServiceImpl userService;
@@ -18,9 +20,18 @@ public class UserController {
     @Value("${test.username}")
     private String username;
 
+    @Value("${test.username2}")
+    private String username2;
+
+    @Value("${test.username3}")
+    private String username3;
+
+
     @GetMapping("/userInfo")
     public Map<String, Object> userVa() throws Exception {
         System.out.println(username);
+        System.out.println(username2);
+        System.out.println(username3);
         return userService.getUserInfo(1);
     }
 
